27d32a27abd1965992c648fd41a98b5ff08de622
Philippa Wood <43877769+PhilippaWood@users.noreply.github.com>
2018-10-28 12:56:41 +0000

    some intro and working map

---
+++ AssigPart1_Rmap1.R
@@ -0,0 +1,18 @@
+library(maptools) 
+library(RColorBrewer) 
+library(classInt) 
+library(OpenStreetMap) 
+library(sp) 
+library(rgeos) 
+library(tmap) 
+library(tmaptools) 
+library(sf) 
+library(rgdal) 
+library(geojsonio)
+library(tidyverse)
+library(ggplot2)
+
+##https://www.gl-li.com/2018/01/17/create-pie-plots-on-a-map-in-r/ last accessed 28/10/18
+
+install.packages("scatterpie")
+install.packages("dplyr")
